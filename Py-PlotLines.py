ddf18cfadb31832ed87fb7938f7e258279c0b865
Jirka Neumann <jirka.neumann@gmx.de>
2025-03-08 01:24:19 +0100

    new lines 2 script

--- Py-PlotLines.py
+++ Py-PlotLines.py
@@ -1,9 +1,3 @@
-def prinje(**kwargs):
-  print("+-----------------------------------------")
-  for key, value in kwargs.items():
-    print(f"| Var: {key} | Val: {value}")
-  print("+-----------------------------------------")
-
 def loogress(first, last):
   print("\r", f"working: {first+1} of {last} --> {round((first+1)/(last/100), 1)}%", end="")
   if first+1 == last: print("\n done! =)\n")
@@ -20,3 +14,42 @@ except:
   path = ""
   try: import ctypes; ctypes.windll.user32.MessageBoxW(0, "check path...", "Python", 1)
   except: print("check path...")
+
+print(path)
+print("")
+print(getnotes)
+
+#------------------------------------------------------------------#
+#---------------------------- get data ----------------------------#
+try:
+  with open("sample.txt", "r", encoding="utf-8") as data:
+    getdata = []
+    for lines in data:
+      getdata.append(lines.strip("\r\n"))
+    data.close()
+except:
+  print("no data")
+
+
+print(getdata)
+
+iterations = len(getdata)
+
+for i in range(iterations):
+  print(getdata[i])
+  #exec('try:loogress(i, iterations)\nexcept:print("working...")')
+
+import re
+#pattern = r"^Titel:"
+pattern = "Titel:"
+
+for i in range(iterations):
+  print(getdata[i])
+  if getdata[i].startswith(pattern):
+    print("Hell yeah..!!")
+  #if re.match(pattern, getdata[i]):
+  #  print("Titel found...")
+  #exec('try:loogress(i, iterations)\nexcept:print("working...")')
+
+
+#https://pythonspot.com/matplotlib-bar-chart/
